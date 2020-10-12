1ff822e3692f9b61afbebf288280fcbc5c07efe0
vyshnavi7120 <62387738+vyshnavi7120@users.noreply.github.com>
2020-10-12 11:58:12 +0530

    saveddata

--- file/data.php
+++ file/data.php
@@ -3,35 +3,21 @@
 require_once ("db.php");
 require_once ("component.php");
 
-$con = Createdb();
+$con = Savedb();
 
-// create button click
-if(isset($_POST['create'])){
-    createData();
+if(isset($_POST['Save details'])){
+    savedData();
 }
 
-if(isset($_POST['update'])){
-    UpdateData();
-}
+function savedData(){
+    $name = textboxValue("name");
+    $city = textboxValue("city");
+    $occupation = textboxValue("occupation");
 
-if(isset($_POST['delete'])){
-    deleteRecord();
-}
+    if($name && $city && $occupation){
 
-if(isset($_POST['deleteall'])){
-    deleteAll();
-
-}
-
-function createData(){
-    $bookname = textboxValue("book_name");
-    $bookpublisher = textboxValue("book_publisher");
-    $bookprice = textboxValue("book_price");
-
-    if($bookname && $bookpublisher && $bookprice){
-
-        $sql = "INSERT INTO books (book_name, book_publisher, book_price) 
-                        VALUES ('$bookname','$bookpublisher','$bookprice')";
+        $sql = "INSERT INTO books (name, city, occupation) 
+                        VALUES ('$name','$city','$occupation')";
 
         if(mysqli_query($GLOBALS['con'], $sql)){
             TextNode("success", "Record Successfully Inserted...!");
@@ -53,17 +39,8 @@ function textboxValue($value){
     }
 }
 
-
-// messages
-function TextNode($classname, $msg){
-    $element = "<h6 class='$classname'>$msg</h6>";
-    echo $element;
-}
-
-
-// get data from mysql database
 function getData(){
-    $sql = "SELECT * FROM books";
+    $sql = "SELECT * FROM formdata";
 
     $result = mysqli_query($GLOBALS['con'], $sql);
 
@@ -72,74 +49,7 @@ function getData(){
     }
 }
 
-// update dat
-function UpdateData(){
-    $bookid = textboxValue("book_id");
-    $bookname = textboxValue("book_name");
-    $bookpublisher = textboxValue("book_publisher");
-    $bookprice = textboxValue("book_price");
 
-    if($bookname && $bookpublisher && $bookprice){
-        $sql = "
-                    UPDATE books SET book_name='$bookname', book_publisher = '$bookpublisher', book_price = '$bookprice' WHERE id='$bookid';                    
-        ";
-
-        if(mysqli_query($GLOBALS['con'], $sql)){
-            TextNode("success", "Data Successfully Updated");
-        }else{
-            TextNode("error", "Enable to Update Data");
-        }
-
-    }else{
-        TextNode("error", "Select Data Using Edit Icon");
-    }
-
-
-}
-
-
-function deleteRecord(){
-    $bookid = (int)textboxValue("book_id");
-
-    $sql = "DELETE FROM books WHERE id=$bookid";
-
-    if(mysqli_query($GLOBALS['con'], $sql)){
-        TextNode("success","Record Deleted Successfully...!");
-    }else{
-        TextNode("error","Enable to Delete Record...!");
-    }
-
-}
-
-
-function deleteBtn(){
-    $result = getData();
-    $i = 0;
-    if($result){
-        while ($row = mysqli_fetch_assoc($result)){
-            $i++;
-            if($i > 3){
-                buttonElement("btn-deleteall", "btn btn-danger" ,"<i class='fas fa-trash'></i> Delete All", "deleteall", "");
-                return;
-            }
-        }
-    }
-}
-
-
-function deleteAll(){
-    $sql = "DROP TABLE books";
-
-    if(mysqli_query($GLOBALS['con'], $sql)){
-        TextNode("success","All Record deleted Successfully...!");
-        Createdb();
-    }else{
-        TextNode("error","Something Went Wrong Record cannot deleted...!");
-    }
-}
-
-
-// set id to textbox
 function setID(){
     $getid = getData();
     $id = 0;
@@ -150,3 +60,4 @@ function setID(){
     }
     return ($id + 1);
 }
+?>
